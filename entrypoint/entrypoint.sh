de19d832c035785ac905c94fa575d5f8f0299259
jun-is-duck <junisduck@gmail.com>
2023-02-06 23:03:10 +0900

    ci build test#59

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # `$#` expands to the number of arguments and `$@` expands to the supplied `args`
-printf '%d args:' "$#"
-printf " '%s'" "$@"
-printf '\n'
+#printf '%d args:' "$#"
+#printf " '%s'" "$@"
+#printf '\n'
 
+set -e
+
+ls
+
+exec "$@"
 # chmod 755 /entrypoint.sh
